ac0f03bd66577089f77b405609e22e11b5abc166
Alex García <alexeiiw@gmail.com>
2026-03-19 16:08:19 -0600

    Add initial hello world example in C#
    This program prints a greeting message and some information about C#.

---
+++ 01_ejemplos/ejemplo_hola_mundo/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Console.WriteLine imprime el texto y salta a la siguiente línea
+        Console.WriteLine("------------------------------------------");
+        Console.WriteLine("        EJEMPLO DE SALUDO INICIAL         ");
+        Console.WriteLine("------------------------------------------");
+
+        Console.WriteLine("Hola Mundo desde la Sede Portales.");
+        Console.WriteLine("C# es un lenguaje potente y estructurado.");
+        
+        Console.WriteLine("\nPresiona cualquier tecla para finalizar...");
+    }
+}
